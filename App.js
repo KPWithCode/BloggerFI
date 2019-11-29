f856c77a2e875bed4f0b486406da20ee5cd2b966
Kyle Phillips <khillips375@yahoo.com>
2019-11-28 20:08:07 -0500

    Implemented automatic context creation

--- App.js
+++ App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack'
 import IndexScreen from './src/screens/indexScreen';
-import { BlogProvider } from './src/context/BlogContext'
+import { Provider } from './src/context/BlogContext'
 
-const navigator = createStackNavigator({
-  
-  Index: IndexScreen
-}, {
-  initialRouterName:'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs'
-  }
-});
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
+  },
+  {
+    initialRouterName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  });
 
 const App = createAppContainer(navigator)
 
 export default () => {
-  return <BlogProvider>
-      <App/>
-    </BlogProvider>
+  return (
+    <Provider>
+      <App />
+    </Provider>
+  )
 }
