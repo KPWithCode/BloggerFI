03614dd3c31b7b46771d92055c026023127d7a12
Kyle Phillips <khillips375@yahoo.com>
2019-11-28 20:49:34 -0500

    Can now delete blog post

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,26 +1,53 @@
-import React, {useContext} from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Entypo } from '@expo/vector-icons';
+
+
 
 const IndexScreen = () => {
-const {state, addBlogPost} = useContext(Context)
-    return(
+
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context)
+    return (
         <View>
-            <Text>Index Screen</Text>
             <Button title="Add Post" onPress={addBlogPost} />
-            <FlatList 
-            data={state} 
-            keyExtractor={blogPost => blogPost.title}
-            renderItem={({item}) => {
-                return <Text>{item.title}</Text>
-            }}
+            <FlatList
+                data={state}
+                keyExtractor={blogPost => blogPost.title}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>
+                                {item.title} - {item.id}
+                            </Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)} >
+                                <Entypo style={styles.icon} name="trash" />
+                            </TouchableOpacity>
+                        </View>
+                    )
+
+                }}
             />
         </View>
     )
 }
 
-const styles =  StyleSheet.create({
-
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: 'gray',
+        paddingHorizontal: 10
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
 })
 
 export default IndexScreen;
