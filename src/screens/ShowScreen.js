7c12ad00532bf7503f0e6284521ea6641d6f4366
Kyle Phillips <khillips375@yahoo.com>
2019-12-01 17:38:40 -0500

    Now includes Edit button with icon of appropriate sizing and positioning

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext'
-
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import {FontAwesome } from '@expo/vector-icons';
 const ShowScreen = ({ navigation }) => {
     const { state } = useContext(Context)
 
@@ -9,12 +10,23 @@ const ShowScreen = ({ navigation }) => {
     return (
         <View>
             <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
         </View>
     )
 
+}
 
+
+ShowScreen.navigationOptions = ({navigation}) => {
+    return {
+        headerRight: <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+            <FontAwesome style={styles.headerRight} name="edit" size={30} />
+        </TouchableOpacity>
+    }
 }
 const styles = StyleSheet.create({
-
+    headerRight: {
+        marginRight:10
+    }
 })
 export default ShowScreen
